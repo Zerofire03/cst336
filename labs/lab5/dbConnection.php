8e1d52179c92a750566530f72be4871f69d1f63f
Chris Holmes <cholmes@csumb.edu>
2018-07-03 19:48:59 +0000

    Print all db variables

--- labs/lab5/dbConnection.php
+++ labs/lab5/dbConnection.php
@@ -12,19 +12,16 @@ function getDatabaseConnection($dbname = 'ottermart')
     {
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $host = $url["host"];
-        print($host . " ");
-        //$dbName = substr($url["path"], 1);
         $dbName = "heroku_53322df5e83175d";
         print($dbName . " ");
         $username = $url["user"];
-        print($username . " ");
         //$password = $url["pass"];
         $password = "fbe93cf6";
-        print($password);
     } 
 
     
     //creates db connection
+    print($host . " " . $dbName . " " . $username . " " . $password);
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     
     //display errors when accessing tables
