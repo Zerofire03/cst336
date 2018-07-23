438c1d13bdf6bc344a7e7e91f9a279b761b7ec27
Chris Holmes <cholmes@csumb.edu>
2018-07-23 20:32:43 +0000

    Lab8 completed

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ echo "Christopher Holmes's CST336 Internet Programming";
 <a href="labs/lab5/">Lab 5</a>
 <a href="labs/lab6/">Lab 6</a>
 <a href="labs/lab7/">Lab 7</a>
+<a href="labs/lab8/">Lab 8</a>
 
 <br /> <br />
 
